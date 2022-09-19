ede0c7290b49802583adb2d0ec538e84d6950d98
MagnoTheFirst <alejandro.r.laneri@gmail.com>
2022-09-19 20:22:47 +0200

    dev branch

--- src/main/java/com/example/cvuploadmodule/CvUploadModuleApplication.java
+++ src/main/java/com/example/cvuploadmodule/CvUploadModuleApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 
 import com.example.cvuploadmodule.StorageService;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -25,4 +28,14 @@ public class CvUploadModuleApplication {
             storageService.init();
         };
     }
+/*
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(900MB);
+        factory.setMaxRequestSize("900MB");
+        return factory.createMultipartConfig();
+    }
+
+ */
 }
